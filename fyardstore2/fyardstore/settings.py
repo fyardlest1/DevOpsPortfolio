c77ae4c06edfa4b7df53da4c4b278d1e47ae89bb
Yardley <fyardlest1@gmail.com>
2022-06-24 16:22:05 -0500

    password change for docker and db

--- fyardstore2/fyardstore/settings.py
+++ fyardstore2/fyardstore/settings.py
@@ -98,17 +98,6 @@ DATABASES = {
     }
 }
 
-# DATABASES = {
-#     'default': {
-#     'ENGINE': 'django.db.backends.postgresql_psycopg2',
-#     'NAME': 'fyardstore2_db', 
-#     'USER': 'postgres', 
-#     'PASSWORD': 'YoodY5.0,',
-#     'HOST': '127.0.0.1', 
-#     'PORT': '5433',
-#     }
-# }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/4.0/ref/settings/#auth-password-validators
